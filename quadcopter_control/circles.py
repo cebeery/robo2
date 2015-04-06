32fc23979a089cc0660b1c29acea3370beb05efb
Sarah Walters <sarah.walters@students.olin.edu>
2015-04-06 15:08:27 -0400

    Using self.twist instead of Twist()

--- quadcopter_control/circles.py
+++ quadcopter_control/circles.py
@@ -10,15 +10,14 @@ class CircleDemo(RosNode):
 
     def switch(self, elapsed):
         if elapsed < 5: # go straight up
-            twist = Twist()
-            twist.linear.z = 1
-            self.pub.publish(twist)
+            self.twist.linear.z = 1
+            self.pub.publish(self.twist)
             self.rate.sleep()
         elif elapsed >= 5: # trace circle
-            twist = Twist()
-            twist.linear.x = 1 # translate
-            twist.angular.z = 36 # spin
-            self.pub.publish(twist)
+            self.twist = Twist()
+            self.twist.linear.x = 1 # translate
+            self.twist.angular.z = 36 # spin
+            self.pub.publish(self.twist)
             self.rate.sleep()
 
 if __name__ == "__main__":
